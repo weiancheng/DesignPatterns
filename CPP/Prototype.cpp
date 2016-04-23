36c679c3dd7f0b049f161c7582883ed01c249cc4
Weian Cheng <weian_cheng@htc.com>
2016-04-23 11:27:34 +0800

    add Prototype Pattern.

---
+++ CPP/Prototype.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+
+enum ELANGUAGE {
+    EENGLISH = 1,
+    ECHINESS = 2,
+    EJAPANESE = 3,
+};
+
+class Book {
+public:
+    virtual ~Book(void) {}
+
+    virtual void setName(std::string name) = 0;
+    virtual void setPages(int page) = 0;
+    virtual void setLanguage( ELANGUAGE language ) = 0;
+    virtual void showDetail(void) = 0;
+    virtual Book * clone(void) = 0;
+
+protected:
+    std::string mName;
+    int mPages;
+    ELANGUAGE mLanguage;
+};
+
+class Magazine : public Book {
+public:
+    virtual void setName(std::string name) {
+        mName = name;
+    }
+
+    virtual void setPages(int page) {
+        mPages = page;
+    }
+
+    virtual void setLanguage(ELANGUAGE language) {
+        mLanguage = language;
+    }
+
+    virtual void showDetail(void) {
+        std::cout << "Book Type: Novel" << std::endl;
+        std::cout << "Book Name: " << mName << std::endl;
+        std::cout << "Book Page: " << mPages << std::endl;
+        std::cout << "Book Language: ";
+        switch (mLanguage) {
+            case ECHINESS:
+                std::cout << "Chiness" << std::endl;
+                break;
+
+            case EENGLISH:
+                std::cout << "English" << std::endl;
+                break;
+
+            case EJAPANESE:
+                std::cout << "Japanese" << std::endl;
+                break;
+
+            default:
+                std::cout << "Other" << std::endl;
+        }
+    }
+
+    virtual Book * clone(void) {
+        Book * book = new Magazine();
+        book->setName(this->mName);
+        book->setPages(this->mPages);
+        book->setLanguage(this->mLanguage);
+
+        return book;
+    }
+};
+
+class Novel : public Book {
+public:
+    virtual void setName(std::string name) {
+        mName = name;
+    }
+
+    virtual void setPages(int page) {
+        mPages = page;
+    }
+
+    virtual void setLanguage(ELANGUAGE language) {
+        mLanguage = language;
+    }
+
+    virtual void showDetail(void) {
+        std::cout << "Book Type: Novel" << std::endl;
+        std::cout << "Book Name: " << mName << std::endl;
+        std::cout << "Book Page: " << mPages << std::endl;
+        std::cout << "Book Language: ";
+        switch (mLanguage) {
+            case ECHINESS:
+            std::cout << "Chiness" << std::endl;
+                break;
+
+            case EENGLISH:
+            std::cout << "English" << std::endl;
+                break;
+
+            case EJAPANESE:
+            std::cout << "Japanese" << std::endl;
+                break;
+
+            default:
+            std::cout << "Other" << std::endl;
+        }
+    }
+
+    virtual Book * clone(void) {
+        Book * book = new Novel();
+        book->setName(this->mName + " Copy");
+        book->setPages(this->mPages);
+        book->setLanguage(this->mLanguage);
+
+        return book;
+    }
+};
+
+int main(int argc, char **argv) {
+    Book * HarryPotter = new Novel();
+    Book * HarryBatter = nullptr;
+
+    HarryPotter->setName("Harry Potter");
+    HarryPotter->setLanguage(EENGLISH);
+    HarryPotter->setPages(500);
+    HarryPotter->showDetail();
+
+    HarryBatter = HarryPotter->clone();
+    std::cout << std::endl;
+    HarryBatter->showDetail();
+
+    delete(HarryPotter);
+    delete(HarryBatter);
+
+    return 0;
+}
